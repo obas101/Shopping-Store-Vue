514b72acc8c0d2dc8a041414f98851a8b8339e26
Obadoba Yunusa <obadoba.yunusa@e4email.net>
2022-08-16 22:08:31 +0100

    refactor

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,30 +1,38 @@
 <template>
-    <div>
-        <h4><strong>Categories</strong></h4>
-        <b-list-group>
-            <b-list-group-item class="list-style" href="#">Pams</b-list-group-item>
-            <b-list-group-item class="list-style" href="#">Sandals</b-list-group-item>
-            <b-list-group-item class="list-style" href="#">Casual</b-list-group-item>
-            <b-list-group-item class="list-style" href="#">Boots</b-list-group-item>
-            <b-list-group-item class="list-style" href="#">Formal Shoes</b-list-group-item>
-            <b-list-group-item class="list-style" href="#">Loafers and Slip-ons</b-list-group-item>
-            <b-list-group-item class="list-style" href="#">Canvas</b-list-group-item>
-            <b-list-group-item class="list-style" href="#">Boat Shoes</b-list-group-item>
-            <b-list-group-item class="list-style" href="#">Work and Safety</b-list-group-item>
-        </b-list-group>
-    </div>
+  <div>
+    <h4><strong>Categories</strong></h4>
+    <b-list-group>
+      <b-list-group-item class="list-style" href="#">Pams</b-list-group-item>
+      <b-list-group-item class="list-style" href="#">Sandals</b-list-group-item>
+      <b-list-group-item class="list-style" href="#">Casual</b-list-group-item>
+      <b-list-group-item class="list-style" href="#">Boots</b-list-group-item>
+      <b-list-group-item class="list-style" href="#"
+        >Formal Shoes</b-list-group-item
+      >
+      <b-list-group-item class="list-style" href="#"
+        >Loafers and Slip-ons</b-list-group-item
+      >
+      <b-list-group-item class="list-style" href="#">Canvas</b-list-group-item>
+      <b-list-group-item class="list-style" href="#"
+        >Boat Shoes</b-list-group-item
+      >
+      <b-list-group-item class="list-style" href="#"
+        >Work and Safety</b-list-group-item
+      >
+    </b-list-group>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Categories'
-}
+  name: "Categories",
+};
 </script>
 
 <style scoped>
 .list-style {
-    background: #212529;
-    color: white;
-    text-align: left;
+  background: #212529;
+  color: white;
+  text-align: left;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,17 @@
 <template>
-<nav>
-  <v-toolbar flat app background-color="grey">
-    <v-toolbar-title class="text-uppercase grey--text !important" >
-      <router-link to="/">
-      <span class="font-weight-light">Just</span>
-      <span class="font-weight-bold">Shoes</span>
-      </router-link>    
-    </v-toolbar-title>
-    <v-spacer></v-spacer>
-      <v-text-field
+  <nav>
+    <v-toolbar background-color="grey">
+      <v-toolbar-title class="text-uppercase grey--text !important">
+        <router-link to="/">
+          <span class="font-weight-light">Just</span>
+          <span class="font-weight-bold">Shoes</span>
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <!-- search bar -->
+      <searchbar />
+
+      <!-- <v-text-field
         placeholder="Search"
         rounded
         dense
@@ -17,43 +20,40 @@
         class="shrink mt-6"
         color="#FFDA00"
         active
-      ></v-text-field>
+      ></v-text-field> -->
+      <v-spacer></v-spacer>
 
-    <v-btn flat text>
-    <span class="font-weight-medium">Sign In</span>
-  </v-btn>
+      <v-btn text>
+        <span class="font-weight-medium">Sign In</span>
+      </v-btn>
 
-    <v-btn flat text>
-    <span class="font-weight-medium">Sign Up</span>
-  </v-btn>
-  
-  <v-btn flat text>
-    <v-icon left color="#FFDA00">shopping_cart</v-icon>
-    <span class="font-weight-medium">Cart</span>
-  </v-btn>
-  </v-toolbar>
-</nav>
+      <v-btn flat text>
+        <span class="font-weight-medium">Sign Up</span>
+      </v-btn>
 
-
-
+      <v-btn flat text>
+        <v-icon left color="#FFDA00">shopping_cart</v-icon>
+        <span class="font-weight-medium">Cart</span>
+      </v-btn>
+    </v-toolbar>
+  </nav>
 </template>
 
 <script>
+import Searchbar from "../components/Searchbar.vue";
 export default {
-    name: 'Header'
-    
-}
+  name: "Header",
+  components: { Searchbar },
+};
 </script>
 
 <style scoped>
 .headerdes {
-  background-color: red
-} 
-
-
-.fa-shoe-prints {
-  color:white;
-  font-size: 1rem;
+  background-color: red;
 }
 
+.fa-shoe-prints {
+  color: white;
+  font-size: 1rem;
+}
 </style>
